03ff08dc049f7c965540edeb2e3e84d142cf7503
Nicholas Keller <nic.keller328@gmail.com>
2020-03-30 17:59:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(number)
   range = (1..round).to_a
   i = 0
   if number == 1 || number == 2
-    true
+    return true
   else
     while i < range.length
       if (number / range[i]).is_a? Integer
