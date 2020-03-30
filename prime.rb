bbc8004da51b154a09e0e4332e1655d3351f6916
Nicholas Keller <nic.keller328@gmail.com>
2020-03-30 17:48:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,13 @@
 def prime?(number)
   square_root = number / number
-  
-  
+  round = square_root.round
+  range = (1..round).to_a
+  i = 0
+  while i < range.length
+    if (number / range[i]).is? Integer
+      false
+    else
+      true
+    end  
+    i += 1
 end
