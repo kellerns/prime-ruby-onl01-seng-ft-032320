7aa569520cc633fae30dd19b13751d8133dcbf8f
Nicholas Keller <nic.keller328@gmail.com>
2020-03-30 18:11:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,16 +3,16 @@ def prime?(number)
   round = square_root.round
   range = (1..round).to_a
   i = 0
+  
   if number == 1 || number == 2
     return true
   else
-    while i < range.length
-      if (number / range[i]).is_a?(Integer)
-        false
-      else
-        true
-      end  
-      i += 1
-    end
+    tf_array = []
+  while i < (range.length - 1)
+    tf_array << (number / range[i]).is_a?(Integer)
+    i += 1
+  end
+
+  tf_array.all? false
   end
 end
